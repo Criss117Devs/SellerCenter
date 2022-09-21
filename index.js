3a7f5065a9af00fc14a800d9ce65e9dba4ef8154
cristian.gutierrez@puntocommerce.com <nevve48@gmail.com>
2022-09-20 19:22:55 -0500

    deploy to vercel-3

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  
+ res.json({ message: "from index api" });
+
+});
+
+
+app.listen(8080, () => {
+
+  console.log(`Server is running`);
+
+
+});
